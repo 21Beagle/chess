b13fe3d74de7dd654e17752f4d36b009f57e32be
Jack Beagle <jackbeagle@live.co.uk>
2023-11-11 03:16:30 +0000

    move to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from "react";
 import Board from "./Components/Board/Board";
 import "./App.css";
+import './App.css'
 
 function App() {
-    return (
-        <div className="App">
+
+  return (
+    <>
+<div className="App">
             <h1 className="App-title">Chess</h1>
             <Board />
         </div>
-    );
+    </>
+  )
 }
 
-export default App;
+export default App
